3e15bfa996a4ea58189e2a0c797a263fe63e29b9
chupapi951 <tankegorionov@gmail.com>
2025-02-09 12:01:52 +0300

    Delete Fishing_2072 directory

--- Fishing_2072/content.js
+++
@@ -1,6 +0,0 @@
-// Ждем сообщения от background.js
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.action === "phishingWarning") {
-        alert(`⚠️ Внимание! Этот сайт может быть фишинговым!\n\nОн похож на: ${message.whiteUrl}`);
-    }
-});
